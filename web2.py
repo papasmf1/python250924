cc6a399082b71a07865ebcbccd8288fcb7ac3514
papasmf1 <papasmf1@gmail.com>
2025-09-24 13:32:08 +0900

    크롤링 연습3

---
+++ web2.py
@@ -0,0 +1,32 @@
+# web2.py
+#클리앙 웹사이트 중고장터 
+from bs4 import BeautifulSoup
+import urllib.request
+import re 
+
+#파일에 저장
+f = open("clien.txt", "wt", encoding="utf-8")
+
+url = "https://www.clien.net/service/board/sold"
+#페이지 실행 결과 문자열 
+data = urllib.request.urlopen(url)
+#스프 객체 생성
+soup = BeautifulSoup(data, 'html.parser')
+
+#중고장터매물 제목
+list = soup.find_all("span", 
+    attrs={"data-role":"list-title-text"})
+
+for item in list:
+    #문자열 가공 
+    title = item.text.strip()
+    print(title)
+    f.write(title + "\n")
+
+#1열에서 코딩 
+f.close()
+
+#선택한 블럭 주석: ctrl + /
+# <span class="subject_fixed" data-role="list-title-text" title="아이폰15프로 화이트 256GB팝니다(애캐플26.04.24까지)">
+# 			아이폰15프로 화이트 256GB팝니다(애캐플26.04.24까지)
+# </span>
